fdda4ca90b746a9f78e0dd384220870fdc46da8b
Troy Holsapple <troy.holsapple@gmail.com>
2018-10-18 14:49:43 -0700

    Automated Project Setup
    * Directory structure automatically created for data
    * Automated formatting of .au audio file to .wav
    * Split training (70%) and testing (30%) data

---
+++ config.py
@@ -0,0 +1,22 @@
+
+class MusicMLConfig:
+
+    ROOT_DIR = "/Users/tholsapp/workstation/sjsu/cs161/MusicML"
+    RAW_DATA_DIR = ROOT_DIR + "/music_ml_backend/resources/data/raw/genres"
+    TRAIN_DATA_DIR = ROOT_DIR + "/music_ml_backend/resources/data/training"
+    TEST_DATA_DIR = ROOT_DIR + "/music_ml_backend/resources/data/test"
+
+
+    GENRE_NAMES = [
+            'blues',
+            'classical',
+            'country',
+            'disco',
+            'hiphop',
+            'jazz',
+            'metal',
+            'pop',
+            'reggae',
+            'rock',
+            ]
+
